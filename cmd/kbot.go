5cf5e29128aec5bb701c216bc863a7d4b4caff61
Oleksandr T <oleksandr.tkachov.ua@gmail.com>
2023-05-10 20:12:23 +0300

    Coding Session 2.5

--- cmd/kbot.go
+++ cmd/kbot.go
@@ -47,9 +47,17 @@ to quickly create a Cobra application.`,
 		kbot.Handle(telebot.OnText, func(m telebot.Context) error {
 
 			log.Print(m.Message().Payload, m.Text())
+			payload := m.Message().Payload
+
+			switch payload {
+			case "hello":
+				err = m.Send(fmt.Sprintf("Hello. I'm Kbot %s!", appVersion))
+			}
 
 			return err
 		})
+
+		kbot.Start()
 
 	},
 }
