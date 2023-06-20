06f516ed0ecc1133b689ca104b3ff626bdbe3d38
Oleksandr T <oleksandr.tkachov.ua@gmail.com>
2023-06-20 18:48:12 +0300

    Update pipeline

--- pipeline/jenkins.groovy
+++ pipeline/jenkins.groovy
@@ -22,7 +22,41 @@ pipeline {
                 echo "Build for platform ${params.OS}"
 
                 echo "Build for arch: ${params.ARCH}"
-
+            }
+        }
+        stage("clone") {
+            steps {
+                echo 'CLONE REPOSITORY'
+                git branch: "${BRANCH}", url: "${REPO}"
+            }
+        }
+        stage("test") {
+            steps {
+                echo 'TEST EXECUTION STARTED '
+                sh 'make test'
+            }
+        }
+        stage("build") {
+            steps {
+                echo 'BUILD EXECUTION STARTED '
+                sh 'make build'
+            }
+        }
+        stage("image") {
+            steps {
+                script {
+                    echo 'BUILD IMAGES EXECUTION STARTED '
+                    sh 'make image'
+                }
+            }
+        }
+        stage("push") {
+            steps {
+                script {
+                    docker.withRegistry('', 'dockerhub') {
+                        sh 'make push'
+                    }
+                }
             }
         }
     }
